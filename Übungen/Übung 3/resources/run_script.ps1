a8084758af8ed66021a828dfd0d234ebb0459805
No_Valis <novalis@posteo.de>
2017-11-06 14:45:41 +0100

    changes

--- Übungen/Übung 3/resources/run_script.ps1
+++ Übungen/Übung 3/resources/run_script.ps1
@@ -3,13 +3,13 @@
 Login-AzureRmAccount
 
 # Import module
-Import-Module "D:\Workspace\WCIS Vorlesung WS17\Übungen\Übung 3\resources\adduser.psm1"
+Import-Module "xxx\adduser.psm1"
 
 
 # Run function in module
-$gatewayIDLeader = "/subscriptions/50a79087-dc66-45d2-bad2-37af46e2dd03/resourceGroups/Ethereum/providers/Microsoft.Network/virtualNetworkGateways/vNet0-Gateway"
-$gatewayIDMember = "/subscriptions/50a79087-dc66-45d2-bad2-37af46e2dd03/resourceGroups/EthereumMember/providers/Microsoft.Network/virtualNetworkGateways/vNet1-Gateway"
-$connectionName = "connection-jozessin"
-$sharedKey = "WCIS2017"
+$gatewayIDLeader = "/subscriptions/xxx"
+$gatewayIDMember = "/subscriptions/xxx"
+$connectionName = "xxx"
+$sharedKey = "xxx"
 
 CreateConnection -MyGatewayResourceId $gatewayIDLeader -OtherGatewayResourceId $gatewayIDMember -ConnectionName $connectionName -SharedKey $sharedKey
